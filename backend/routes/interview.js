a50c041277858f651137824b445cbbf54a3c4197
yiyanggis <yiyanggis@gmail.com>
2020-11-26 02:58:12 -0800

    return question content

--- backend/routes/interview.js
+++ backend/routes/interview.js
@@ -15,4 +15,14 @@ router.get('/test', function(req, res, next) {
   res.send('test');
 });
 
+router.get('/question/:questionId', function(req, res, next){
+	if(questionId != 1){
+		res.send({"error":"no data"});
+	}
+	var data = {
+		question: "implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?"
+	}
+	res.send(data)
+})
+
 module.exports = router;
